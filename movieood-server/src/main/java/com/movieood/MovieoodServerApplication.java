aab739d5bda1c5b90f76d7bbeb73d92925f7d4bb
sungAh123 <bobaah0832@gmail.com>
2025-11-23 17:17:54 +0900

    Initial commit

---
+++ movieood-server/src/main/java/com/movieood/MovieoodServerApplication.java
@@ -0,0 +1,13 @@
+package com.movieood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieoodServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieoodServerApplication.class, args);
+    }
+
+}
---
+++ movieood-server/src/test/java/com/movieood/MovieoodServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieoodServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
